02d745081f35d22e19257efca91f180fde1a7f39
kishoreismac <27953059+kishoreismac@users.noreply.github.com>
2026-02-05 21:28:16 +0530

    adding code files to the repo

---
+++ HelloWorld.cs
@@ -0,0 +1,23 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloDotNetFunc;
+
+public class HelloWorld
+{
+    private readonly ILogger<HelloWorld> _logger;
+
+    public HelloWorld(ILogger<HelloWorld> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("HelloWorld")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
